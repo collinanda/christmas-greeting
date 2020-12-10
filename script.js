1cedb72eb6346bc559d7c01980042957e6223033
collinanda <71196128+collinanda@users.noreply.github.com>
2020-12-09 19:31:59 -0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-let envelope = document.getElementById('envelope');
-
-function showLetter() {
-    let letter = document.getElementById('letter');
-    letter.classList.add('letter');
-    
-}
-
-envelope.addEventListener('click', showLetter);
